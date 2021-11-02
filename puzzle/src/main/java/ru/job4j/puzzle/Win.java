83a6d80f72dd663fc087d5e65fe8474f41839af0
sayrus89 <sayrus5767@mail.ru>
2021-11-02 18:25:31 +0500

    2. Сокобан. Тестовое задание. [#75283] исправление. if (board[i][i] == 1 && (Win.horizontal(board, i) || Win.vertical(board, i)))

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,7 +4,7 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (Win.horizontal(board, i) || Win.vertical(board, i)) {
+            if (board[i][i] == 1 && (Win.horizontal(board, i) || Win.vertical(board, i))) {
                 rsl = true;
                 break;
             }
